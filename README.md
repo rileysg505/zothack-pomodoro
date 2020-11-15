c7c1129a957ef5701f3bcc031168e06a6281972d
rileysg505 <59489097+rileysg505@users.noreply.github.com>
2020-11-15 08:17:11 -0800

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# ZotHacks 2020 Front-end Repository
-
-This is the project repository for the ZotPomodoro, a website made by Jane Vo, Irania Mazariegos, and Riley Garcia
-
-## Purpose
-
-This is a simple timer that uses the Pomodoro technique. It's purpose is to have poeple focus on a task for 25 minutes, followed by a 5 minute break. The user then repreats this multiple times cand this allows for a more efficient workflow
-
---
+++ precache-manifest.adbd817a79bc7e91556b17993e8253c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51ede1c5909860462dcf0c23f63174d",
+    "url": "/zothack-pomodoro/index.html"
+  },
+  {
+    "revision": "5ada9f7fab9ab9fcb9f7",
+    "url": "/zothack-pomodoro/static/css/main.719e9258.chunk.css"
+  },
+  {
+    "revision": "491587e30418b8f386a0",
+    "url": "/zothack-pomodoro/static/js/2.a13fefe1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zothack-pomodoro/static/js/2.a13fefe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ada9f7fab9ab9fcb9f7",
+    "url": "/zothack-pomodoro/static/js/main.90f78607.chunk.js"
+  },
+  {
+    "revision": "5da8f50ee0749f4a2007",
+    "url": "/zothack-pomodoro/static/js/runtime-main.1d87f2de.js"
+  }
+]);
